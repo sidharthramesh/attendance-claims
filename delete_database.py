42b476c8cb456689244756990980fbfd80ad5a41
Sidharth Ramesh <tornadoalert@gmail.com>
2017-04-24 21:15:39 +0530

    modular delete database

--- delete_database.py
+++ delete_database.py
@@ -1,6 +1,8 @@
 
 from flask_app import db
 for i in reversed(db.metadata.sorted_tables):
-    print("Deleting {}".format(i))
-    db.session.execute(i.delete())
-    db.session.commit()
+    if input("Delete {} Y/n?".format(i)).lower() in ['y','yes']:
+        print("Deleting {}".format(i))
+        db.session.execute(i.delete())
+        db.session.commit()
+    
