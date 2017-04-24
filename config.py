a55cfb7c5c70073ea365ca5d97e8f9fcc97ff5ea
Sidharth Ramesh <tornadoalert@gmail.com>
2017-04-24 15:02:51 +0530

    yup

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-SQLALCHEMY_DATABASE_URI_ = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 SQLALCHEMY_POOL_RECYCLE = 299
 
-SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
+SQLALCHEMY_DATABASE_URI_ = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
     username="tornadoalert",
     password="databasepassword123",
     hostname="tornadoalert.mysql.pythonanywhere-services.com",
